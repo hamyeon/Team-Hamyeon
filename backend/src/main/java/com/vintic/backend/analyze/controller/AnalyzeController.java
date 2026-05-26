41bab5710d184575749fe85282de0b7195cd8950
jchaeyun <jocheyoon@gmail.com>
2026-05-26 15:30:51 +0900

    [feat/#38] 이미지 수신->S3 저장 후 URL 반환->OpenAI API 호출하는 파이프라인 구축

--- backend/src/main/java/com/vintic/backend/analyze/controller/AnalyzeController.java
+++ backend/src/main/java/com/vintic/backend/analyze/controller/AnalyzeController.java
@@ -1,15 +1,16 @@
 package com.vintic.backend.analyze.controller;
 
+import com.vintic.backend.ai.service.OpenAiService;
+import com.vintic.backend.analyze.service.ProductAnalyzeService;
 import com.vintic.backend.analyze.service.S3UploaderService;
 import com.vintic.backend.common.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+
 
 //S3UploaderService에 프론트가 보낸 이미지를 전달하는 컨트롤러
 @RestController
@@ -17,9 +18,11 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class AnalyzeController {
 
+    private final OpenAiService openAiService;
+    private final ProductAnalyzeService productAnalyzeService;
     private final S3UploaderService s3UploaderService;
 
-    @PostMapping("/analyze")
+    @PostMapping(value = "/analyze", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> analyzeImage(@RequestPart("image") MultipartFile image) {
         // 빈 파일이 오면 쫓아냄
         if (image == null || image.isEmpty()) {
@@ -30,8 +33,11 @@ public class AnalyzeController {
             // 서비스 호출 (이미지 S3 저장 후 URL 반환)
             String imageUrl = s3UploaderService.uploadImage(image);
 
+            // 획득한 S3 URL을 OpenAI API에 전달
+            String aiAnalysisResult = openAiService.analyzeProductImage(imageUrl);
 
-            return ResponseEntity.ok(ApiResponse.success(imageUrl));
+            // 최종 AI 분석 결과를 기존 공통 ApiResponse 성공 포맷에 담아 반환
+            return ResponseEntity.ok(ApiResponse.success(aiAnalysisResult));
 
         } catch (Exception e) {
             // 업로드 중 에러가 터지면 500 에러 반환
