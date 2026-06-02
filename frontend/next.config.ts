c68b717caa5b1b98007ca88490afd603eb9e92e4
suyeon <yebbi2@ewhain.net>
2026-06-02 22:45:21 +0900

    [fix/#84] 오타 수정

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
-const backendBaseUrl = process.env.BACKEND_BACE_URL;
+const backendBaseUrl = process.env.BACKEND_BASE_URL;
 
 if (!backendBaseUrl) {
   throw new Error('BACKEND_BASE_URL 환경변수가 설정되지 않았습니다.')
